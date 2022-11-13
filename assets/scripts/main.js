8147769532fc3c3769123eb5f53a827944aaa919
Henry Feng <henryfeng21@gmail.com>
2022-11-13 02:07:33 -0800

    Lab 7 changes

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -54,6 +54,17 @@ function initializeServiceWorker() {
   // B5. TODO - In the event that the service worker registration fails, console
   //            log that it has failed.
   // STEPS B6 ONWARDS WILL BE IN /sw.js
+  if ("serviceWorker" in navigator) {
+    window.addEventListener('load', async (event) => {
+      try {
+        const registration = await navigator.serviceWorker.register("/sw.js");
+        if (registration.active) console.log("active")
+      }
+      catch (error) {
+        console.error(`failed with ${error}`);
+      }
+    })
+  }
 }
 
 /**
@@ -68,15 +79,33 @@ async function getRecipes() {
   // EXPOSE - START (All expose numbers start with A)
   // A1. TODO - Check local storage to see if there are any recipes.
   //            If there are recipes, return them.
+  if (localStorage.getItem('recipes') != null) return JSON.parse(localStorage.getItem('recipes'));
   /**************************/
   // The rest of this method will be concerned with requesting the recipes
   // from the network
   // A2. TODO - Create an empty array to hold the recipes that you will fetch
+  const recipes = [];
   // A3. TODO - Return a new Promise. If you are unfamiliar with promises, MDN
   //            has a great article on them. A promise takes one parameter - A
   //            function (we call these callback functions). That function will
   //            take two parameters - resolve, and reject. These are functions
   //            you can call to either resolve the Promise or Reject it.
+  return new Promise(async (resolve, reject) => {
+    try {
+      for (let i = 0; i < RECIPE_URLS.length; i++) {
+        const request = new Request(RECIPE_URLS[i]);
+        let response = await fetch(request);
+
+        recipes.push(await response.json());
+        saveRecipesToStorage(recipes);
+        resolve(recipes);
+      }
+    }
+    catch (error) {
+      console.error(error);
+      reject(error);
+    }
+  });
   /**************************/
   // A4-A11 will all be *inside* the callback function we passed to the Promise
   // we're returning
